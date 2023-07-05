fc38162822965e4cf4644c25018537d0997882d9
kimkiyong <fenderst0612@gmail.com>
2023-07-05 22:04:23 +0900

    chore: update pathpida

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -1,5 +1,12 @@
 export const pagesPath = {
-  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash }),
+  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
 }
 
 export type PagesPath = typeof pagesPath
+
+export const staticPath = {
+  next_svg: '/next.svg',
+  vercel_svg: '/vercel.svg'
+} as const
+
+export type StaticPath = typeof staticPath
