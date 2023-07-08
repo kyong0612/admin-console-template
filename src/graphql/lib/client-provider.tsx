5d5ae22acba2f83598d0e57f3269811df425a159
kimkiyong <fenderst0612@gmail.com>
2023-07-08 22:50:58 +0900

    feat: apollo client

---
+++ src/graphql/lib/client-provider.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { ApolloLink, HttpLink, SuspenseCache } from '@apollo/client'
+import {
+  ApolloNextAppProvider,
+  NextSSRInMemoryCache,
+  NextSSRApolloClient,
+  SSRMultipartLink,
+} from '@apollo/experimental-nextjs-app-support/ssr'
+
+function makeClient() {
+  const httpLink = new HttpLink({
+    // TODO: get from env
+    uri: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
+  })
+
+  return new NextSSRApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === 'undefined'
+        ? ApolloLink.from([
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+          ])
+        : httpLink,
+  })
+}
+
+function makeSuspenseCache() {
+  return new SuspenseCache()
+}
+
+export function ApolloWrapper({ children }: React.PropsWithChildren) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient} makeSuspenseCache={makeSuspenseCache}>
+      {children}
+    </ApolloNextAppProvider>
+  )
+}
