bd0504fac6b68d7cb6727748762c0ccbc3cdb7bb
kimkiyong <fenderst0612@gmail.com>
2023-07-09 19:53:52 +0900

    chore: update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -74,4 +74,19 @@ export default defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
+
+  expect: {
+    // Maximum time expect() should wait for the condition to be met.
+    timeout: 5000,
+
+    toHaveScreenshot: {
+      // An acceptable amount of pixels that could be different, unset by default.
+      maxDiffPixels: 10,
+    },
+
+    toMatchSnapshot: {
+      // An acceptable ratio of pixels that are different to the total amount of pixels, between 0 and 1.
+      maxDiffPixelRatio: 0.1,
+    },
+  },
 })
