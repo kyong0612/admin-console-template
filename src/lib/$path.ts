ac239683b62a05991072ae0686933954a05ccc1b
kimkiyong <fenderst0612@gmail.com>
2023-07-11 21:51:07 +0900

    fix: pathpida

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -1,12 +1,24 @@
 export const pagesPath = {
-  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
+  nested: {
+    nested_sample_page: {
+      $url: (url?: { hash?: string }) => ({
+        pathname: '/nested/nested-sample-page' as const,
+        hash: url?.hash,
+      }),
+    },
+  },
+  sample_page: {
+    $url: (url?: { hash?: string }) => ({ pathname: '/sample-page' as const, hash: url?.hash }),
+  },
+  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash }),
 }
 
 export type PagesPath = typeof pagesPath
 
 export const staticPath = {
+  favicon_ico: '/favicon.ico',
   next_svg: '/next.svg',
-  vercel_svg: '/vercel.svg'
+  vercel_svg: '/vercel.svg',
 } as const
 
 export type StaticPath = typeof staticPath
