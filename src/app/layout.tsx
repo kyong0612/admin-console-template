1ee335194a83c451634de32a7c686365a54c9856
kimkiyong <fenderst0612@gmail.com>
2023-07-14 22:11:05 +0900

    feat: use apollo client in server component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import '@/style/globals.css'
 import { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { ApolloWrapper } from '@/graphql/lib/client-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,7 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='ja' className={inter.className}>
       <body className='flex min-h-screen flex-col items-center justify-between p-24'>
-        <ApolloWrapper>{children}</ApolloWrapper>
+        {children}
       </body>
     </html>
   )
