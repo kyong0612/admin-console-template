7dd18d4d9729a91a3cc51b318b9e3e201dacd1d5
kyong <fenderst0612@gmail.com>
2023-07-14 21:10:56 +0900

    feat: auth middleware (#15)
    * feat: middleware sample
    
    * feat: basic auth

---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization')
+
+  if (basicAuth) {
+    const auth = basicAuth.split(' ')[1]
+    const [user, password] = atob(auth).split(':')
+
+    if (user === process.env.BASIC_USER_NAME && password === process.env.BASIC_PASSWORD) {
+      return NextResponse.next()
+    }
+  }
+
+  return new Response('Auth required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  })
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/static/:path*|_next/image/:path*|favicon.ico).*)',
+  ],
+}
