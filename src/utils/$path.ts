537760cfb14309f80c81be1fae2223e98bae529a
kimkiyong <fenderst0612@gmail.com>
2023-07-16 19:25:08 +0900

    fix: lint

--- src/utils/$path.ts
+++ src/utils/$path.ts
@@ -1,11 +1,15 @@
 export const pagesPath = {
-  "nested": {
+  nested: {
     _slug: (slug: string | number) => ({
-      $url: (url?: { hash?: string }) => ({ pathname: '/nested/[slug]' as const, query: { slug }, hash: url?.hash })
+      $url: (url?: { hash?: string }) => ({
+        pathname: '/nested/[slug]' as const,
+        query: { slug },
+        hash: url?.hash,
+      }),
     }),
-    $url: (url?: { hash?: string }) => ({ pathname: '/nested' as const, hash: url?.hash })
+    $url: (url?: { hash?: string }) => ({ pathname: '/nested' as const, hash: url?.hash }),
   },
-  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
+  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash }),
 }
 
 export type PagesPath = typeof pagesPath
@@ -13,7 +17,7 @@ export type PagesPath = typeof pagesPath
 export const staticPath = {
   favicon_ico: '/favicon.ico',
   next_svg: '/next.svg',
-  vercel_svg: '/vercel.svg'
+  vercel_svg: '/vercel.svg',
 } as const
 
 export type StaticPath = typeof staticPath
