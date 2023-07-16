ffa7e3efa6071114c2f4150be8c308bf21f60dd0
kimkiyong <fenderst0612@gmail.com>
2023-07-16 16:29:05 +0900

    feat: sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import '@/style/globals.css'
 import { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Providers } from './providers'
+import SimpleSidebar from '@/app/sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,9 +14,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ja' className={inter.className}>
-      <body className='flex min-h-screen flex-col items-center justify-between p-24'>
-        {children}
-      </body>
+      <Providers>
+        <SimpleSidebar>{children}</SimpleSidebar>
+      </Providers>
     </html>
   )
 }
