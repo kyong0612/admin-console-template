e84d49a549ec3a8433778ad8fba3ce38bc990b5c
kyong <fenderst0612@gmail.com>
2023-07-16 18:26:44 +0900

    feat: Chakra UI (#18)
    * chore: update middleware matcher
    
    * feat: pnpm add chakra
    
    * feat: sidebar
    
    * doc: README
    
    * doc: directory architecture
    
    * feat: loading component
    
    * fix: root layout
    
    * chore: remove unused component
    
    * doc: README
    
    * chore: optimize tag
    
    * chore: sidebar
    
    * feat: loading
    
    * feat: remove tailwindcss
    
    * chore: remove comment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import '@/style/globals.css'
 import { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Providers } from './providers'
+import SimpleSidebar from '@/app/sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,8 +14,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ja' className={inter.className}>
-      <body className='flex min-h-screen flex-col items-center justify-between p-24'>
-        {children}
+      <body suppressHydrationWarning={true}>
+        <Providers>
+          <SimpleSidebar>{children}</SimpleSidebar>
+        </Providers>
       </body>
     </html>
   )
---
+++ src/components/loading.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+import { Spinner } from '@chakra-ui/react'
+
+export const LoadingComponent = () => {
+  return <Spinner />
+}
